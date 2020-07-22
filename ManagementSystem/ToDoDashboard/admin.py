2c56b52cbdb0e77f32673aafc9c931f2846c9615
EngineerSpock <EngineerSpock@outlook.com>
2020-07-22 15:26:15 +0300

    1st Module Finished

--- ManagementSystem/ToDoDashboard/admin.py
+++ ManagementSystem/ToDoDashboard/admin.py
@@ -4,6 +4,12 @@ from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin
 
+from ToDoDashboard.models import Member, Dashboard, DashboardColumn, ToDoItem
 from users.models import User
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Member)
+admin.site.register(Dashboard)
+admin.site.register(DashboardColumn)
+admin.site.register(ToDoItem)
